2ca8b344352dfc2dd5600e529d84d7a0cf1962e8
tferrand <tferrand@isep.fr>
2014-05-22 23:11:21 +0200

    commit connection_db.js

--- config/connection_db.js
+++ config/connection_db.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
     user     : 'root',
+    password : 'root',
     host     : 'localhost',
     port: '8889',
     database: 'ieee-cas',
+    dateStrings : true,
 });
 
 exports.initPool = function(){
